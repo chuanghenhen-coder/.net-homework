fdebc5e48c59ab9058e9d7bb6d676c22b6092c5f
chuanghenhen-coder <chuanghenhen@gmail.com>
2026-06-10 14:02:51 +0800

    Update Form1.cs

--- Cloudclipboard/Controller/Form1.cs
+++ Cloudclipboard/Controller/Form1.cs
@@ -1,10 +1,44 @@
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace Controller
 {
     public partial class Form1 : Form
     {
+        NetworkClient networkClient = new NetworkClient();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            await networkClient.ConnectAsync(txtIP.Text);
+            lblStatus.Text = "已連線";
+            lblStatus.ForeColor = Color.Green;
+
+            // 連線後開始在背景接收資料
+            _ = networkClient.StartReceivingAsync(message =>
+            {
+                this.Invoke(() =>
+                {
+                    txtOutput.Text = message;
+                });
+            });
+        }
+
+        private async void btnSendClipboard_Click(object sender, EventArgs e)
+        {
+            await networkClient.SendCommandAsync("clipboard", txtClipboard.Text);
+            lblStatus.Text = "已傳送！";
+        }
+
+        private async void btnSysInfo_Click(object sender, EventArgs e)
+        {
+            await networkClient.SendCommandAsync("sysinfo", "");
+            lblStatus.Text = "已查詢（等待回傳）";
+        }
     }
 }
+
