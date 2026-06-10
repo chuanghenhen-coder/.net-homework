55802f2eca670efe0c572dd78a35c7c31a58f532
hana914628 <fcps1076220@gmail.com>
2026-06-10 12:52:45 +0800

    Add TCP listener for handling client commands
    Implemented a TCP listener to handle incoming client connections and process commands asynchronously.

--- Cloudclipboard/Agent/Program.cs
+++ Cloudclipboard/Agent/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Agent 啟動中...");
+TcpListener listener = new TcpListener(IPAddress.Any, 9000);
+listener.Start();
+Console.WriteLine("等待指令中...");
+
+while (true)
+{
+    TcpClient client = await listener.AcceptTcpClientAsync();
+    _ = HandleClientAsync(client);
+}
+
+async Task HandleClientAsync(TcpClient client)
+{
+    NetworkStream stream = client.GetStream();
+    byte[] buffer = new byte[4096];
+
+    while (true)
+    {
+        int bytesRead = await stream.ReadAsync(buffer);
+        if (bytesRead == 0) break;
+
+        string json = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] 收到指令：{json}");
+
+      
+    }
+}
